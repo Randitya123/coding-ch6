57a465fa9be6924a05543bc8344327275f960de5
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-10-26 09:58:47 +0100

    Add files via upload

---
+++ titanic/titanics.py
@@ -0,0 +1,21 @@
+import pandas as pd
+#dataframe
+df=pd.DataFrame({
+    "NAME":["John","Mike","Adam"],
+    "CITY":["Mumbai","Zurich","Sao Paulo"],
+    "AGE":[3,90,67]
+})
+print(df)
+print(df.head())
+print(df.shape)
+print(df.info())
+print(df.describe())
+#titanic data set
+reads=pd.read_csv("titanic.csv")
+print(reads)
+print(reads.head())
+print(reads.shape)
+print(reads.info())
+print(reads.describe())
+mfc=reads[(reads["Sex"]=="male") & (reads["Pclass"]==1)]
+print(mfc["Fare"].mean())
