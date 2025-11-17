5949d30a75b0467f7ed40befc89979a8d1eb2219
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-11-17 18:57:09 +0100

    Update nestedpiechart.py

--- nestedpiechart.py
+++ nestedpiechart.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 outer=["Protein","carbs","sugar","fat","potassium"]
 outersize=[130,75,0.5,75,3]
-outercolor=["red","green","white","black","yellow"]
+outercolor=["red","green","purple","orange","yellow"]
 inner=["chicken","fish","eggs","bread","potato","pasta","cake","ice-cream","gulab jamun","cheese","oil","avacado","banana","coconut water","spinach"]
 innercolor=["#03fc5e","#0352fc","#fcbe03","#d703fc","#039dfc","#fc3003","#fc035a","#7bfc03","#fc030b","#fc9d03","#03f8fc","#c6fc03","#d703fc","#38a15b","#6e310e"]
-innersize=[62,50,18,30,25,20,0.1,0,2,0.3,10,28,37,0.840,1.2,0.960]
+innersize=[62,50,18,30,25,20,0.1,0.2,0.3,10,28,37,0.840,1.2,0.960]
 fig,ax=plt.subplots(figsize=(9,9))
 #outerring
 ax.pie(outersize,labels=outer,radius=1.3,colors=outercolor,startangle=90,wedgeprops=dict(width=0.3,edgecolor='white'))
