6405fbce995ddea4b63825bc6e1956cb1e03e82d
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2026-01-04 09:57:54 +0100

    Update covid.py

--- c19/covid.py
+++ c19/covid.py
@@ -40,3 +40,47 @@ top10=(read.groupby("country")["recovered"].sum().nlargest(10).sort_values(ascen
 #bubbleplot
 plot=px.scatter(x=top10.index,y=top10.values,size=top10.values,color=top10.index,size_max=120,title="Confirmed total recpvery")
 plot.write_html("plot1.html",auto_open=True)
+def tops(countryname,n=5):
+    countrydata=read[read["country"]==countryname]
+    return countrydata.nlargest(n,"confirmed")
+#usa state analysis
+usas=tops("US")
+figure=go.Figure()
+figure.add_bar(name="confirmed",x=usas["confirmed"],y=usas["state"],orientation="h")
+figure.add_bar(name="deaths",x=usas["deaths"],y=usas["state"],orientation="h")
+figure.update_layout(title="most affected states in usa",barmode="group",height=600)
+figure.write_html("hello.html",auto_open=True)
+
+#india state analysis
+indias=tops("India")
+figure=go.Figure()
+figure.add_bar(name="confirmed",x=indias["confirmed"],y=indias["state"],orientation="h")
+figure.add_bar(name="deaths",x=indias["deaths"],y=indias["state"],orientation="h")
+figure.update_layout(title="most affected states in india",barmode="group",height=600)
+figure.write_html("Title.html",auto_open=True)
+
+#calculating recovery rate and death rate
+read["recover_rate"]=(read["recovered"]/read["confirmed"])*100
+
+read["death_rate"]=(read["deaths"]/read["confirmed"])*100
+
+recover_rate=(read.groupby("country")["recover_rate"].mean().nlargest(5))
+print("top5 countries recovery rate")
+print(recover_rate)
+
+death_rate=(read.groupby("country")["death_rate"].mean().nlargest(5))
+print("top5 countries death rate")
+print(death_rate)
+
+#global covid piechart
+confirmedal=read["confirmed"].sum()
+deathsall=read["deaths"].sum() 
+recoveredall=read["recovered"].sum()
+active=read["active"].sum()
+pies=px.pie(names=["confirmed","deaths","recovered","active"],values=[confirmedal,deathsall,recoveredall,active],title="covid cases")
+pies.write_html("covid-19.html",auto_open=True)
+
+
+
+
+
