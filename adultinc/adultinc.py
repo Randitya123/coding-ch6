335b2b08c7b8d32c28d79973e74b7e6b501de58f
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2026-02-01 09:52:36 +0100

    Update adultinc.py

--- adultinc/adultinc.py
+++ adultinc/adultinc.py
@@ -31,11 +31,15 @@ read["50k"]=read["50k"].map({" >50K":1," <=50K":0})
 
 print(read.head())
 #CONVERTING TEXT DATA TO NUMERICAL DATA
-read["gender"]=read["gender"].map({"Male":0,"Female":1}).astype(int)
-read["race"]=read["race"].map({"White":0,"Black":1,"Asian-Pac-Islander":2,"Amer-Indian-Eskimo":3,"Other":4,}).astype(int)
-read["marry"]=read["marry"].map({"Married-civ-spouse":0,"Never-married":1,"Divorced":2,"Separated":3,"Widowed":4,"Married-spouse-absent":5,"Married-AF-spouse":6,}).astype(int)
-read["org"]=read["org"].map({"Private":0,"Self-emp-not-inc":1,"Local-gov":2,"?":3,"State-gov":4,"Self-emp-inc":5,"Federal-gov":6,"Without-pay":7,"Never-worked":8,}).astype(int)
-read["prof"]=read["prof"].map({"Prof-specialty":0,"Craft-repair":1,"Exec-managerial":2,"Adm-clerical":3,"Sales":4,"Other-service":5,"Machine-op-inspct":6,"?":7,"Transport-moving":8,"Handlers-cleaners":9,"Farming-fishing":10,"Tech-support":11,"Protective-serv":12,"Priv-house-serv":13,"Armed-Forces":14}).astype(int)
-read["fam"]=read["fam"].map({"Husband":0,"Not-in-family":1,"Own-child":2,"Own-child":3,"Unmarried":4,"Wife":5,"Other-relative":6,}).astype(int)
+read["gender"]=read["gender"].map({" Male":0," Female":1}).astype(int)
+read["race"]=read["race"].map({" White":0," Black":1," Asian-Pac-Islander":2," Amer-Indian-Eskimo":3," Other":4,}).astype(int)
+read["marry"]=read["marry"].map({" Married-civ-spouse":0," Never-married":1," Divorced":2," Separated":3," Widowed":4," Married-spouse-absent":5," Married-AF-spouse":6,}).astype(int)
+read["org"]=read["org"].map({" Private":0," Self-emp-not-inc":1," Local-gov":2," ?":3," State-gov":4," Self-emp-inc":5," Federal-gov":6," Without-pay":7," Never-worked":8,}).astype(int)
+read["prof"]=read["prof"].map({" Prof-specialty":0," Craft-repair":1," Exec-managerial":2," Adm-clerical":3," Sales":4," Other-service":5," Machine-op-inspct":6," ?":7," Transport-moving":8," Handlers-cleaners":9," Farming-fishing":10," Tech-support":11," Protective-serv":12," Priv-house-serv":13," Armed-Forces":14}).astype(int)
+read["fam"]=read["fam"].map({" Husband":0," Not-in-family":1," Own-child":2," Own-child":3," Unmarried":4," Wife":5," Other-relative":6,}).astype(int)
+read["edu"]=read["edu"].map({" HS-grad":0," Some-college":1," Bachelors":2," Masters":3," Assoc-voc":4," 11th":5," Assoc-acdm":6," 10th":7," 7th-8th":8," Prof-school":9," 9th":10," 12th":11," Doctorate":12," 5th-6th":13," 1st-4th":14," Preschool":15}).astype(int)
 
 print(read.head())
+#data visuals
+read.groupby("edu").inc.mean().plot(kind="bar")
+plt.show()
